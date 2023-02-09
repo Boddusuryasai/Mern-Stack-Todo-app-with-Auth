725e443221be21ff74a46c7a410646e9333fea24
suryasai <suryasaichanti@gmail.com>
2023-02-09 07:32:02 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors')
 const connectToDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const app = express();
+app.use(cors())
 
 // Middleware
 app.use(express.json());
